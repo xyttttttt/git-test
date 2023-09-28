b6b05767e39edb2ba648becf83c893b7406e27bd
xyt <1604899092@qq.com>
2023-09-28 10:24:32 +0800

    hot-fix commit 2

--- src/main/java/com/xyt/git/GitTest.java
+++ src/main/java/com/xyt/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git by hot-fix");
+        System.out.println("hello git by hot-fix test2");
     }
 }
